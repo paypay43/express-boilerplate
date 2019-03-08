93608d1233abec297a9d81371ec12100f12d8701
Payman Kossari <Paymankossari@gmail.com>
2019-03-07 23:44:42 -0500

    new branch

--- src/app.js
+++ src/app.js
@@ -4,26 +4,30 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/users-router');
 
 const app = express();
 
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+  })
+);
 
-app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
+    response = { error: 'Server error' };
   } else {
     console.error(error);
-    response = { message: error.message, error };
+    response = { error: error.message, object: error };
   }
   res.status(500).json(response);
 });
